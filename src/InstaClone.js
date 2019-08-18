b3db0b95ffc049bac35b7ce696ff44baffa3f84c
Nik Polovenko <nikp@jfrog.com>
2019-08-18 16:23:34 -0700

    blank app

---
+++ src/InstaClone.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Image } from 'react-native'
+
+class InstaClone extends Component {
+
+
+    render(){
+
+        return(
+            <View>
+                <Text>My Instagram CLone</Text>
+            </View>
+        );
+    }
+
+}
+
+export default InstaClone;
+
